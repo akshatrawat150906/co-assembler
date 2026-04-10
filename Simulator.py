225ef3dd345d9f9ed5f408d466f5d4039ced51e4
adityaya-oss <aditya25046@iiitd.ac.in>
2026-04-10 22:40:39 +0530

    Implement additional opcode handling in Simulator.py
    added for i type (addi, etc. and load) and b type

--- Simulator.py
+++ Simulator.py
@@ -1,3 +1,33 @@
+elif opcode == 0x13:
+    imm = signed_32(sign_extend(inst >> 20, 12))
+    
+    if func3 == 0x0: 
+        register[rd] = register[rs1] + imm
+        
+    elif func3 == 0x2: 
+        if signed_32(register[rs1]) < imm:
+            register[rd] = 1
+        else:
+            register[rd] = 0
+            
+    elif func3 == 0x3: 
+        if unsigned_32(register[rs1]) < unsigned_32(imm):
+            register[rd] = 1
+        else:
+            register[rd] = 0
+
+elif opcode == 0x03:
+    imm = signed_32(sign_extend(inst >> 20, 12))
+    addr = (register[rs1] + imm) & 0xFFFFFFFF
+    
+
+    if not (DATA_MIN <= addr <= DATA_MAX or STACK_MIN <= addr <= STACK_MAX):
+        print(f"Memory Access Error Load address are out of bounds")
+        import sys
+        sys.exit(1)
+    register[rd] = data_m.get(addr, 0)
+
+
 elif opcode == 0x67:
     imm = signed_32(sign_extend(inst >> 20, 12))
     target = (register[rs1] + imm) & 0xFFFFFFFF
@@ -38,5 +68,27 @@ elif opcode == 0x23:
             sys.exit(1)
         data_m[addr] = register[rs2]
 
+elif opcode == 0x63:
+    part_a = ((inst >> 31) << 12)
+    part_b = (((inst >> 7) & 1) << 11)
+    part_c = (((inst >> 25) & 0x3F) << 5)
+    part_d = (((inst >> 8) & 0xF) << 1)
 
+    all_bits = part_a| part_b | part_c| part_d
+    
+    evalue = sign_extend(all_bits, 13)
+    imm = signed_32(evalue)
+
+    take_branch = False
+    if func3 == 0x0: take_branch = (register[rs1] == register[rs2])
+    elif func3 == 0x1: take_branch = (register[rs1] != register[rs2])
+    elif func3 == 0x4: take_branch = (signed_32(register[rs1]) < signed_32(register[rs2]))
+    elif func3 == 0x5: take_branch = (signed_32(register[rs1]) >= signed_32(register[rs2]))
+    elif func3 == 0x6: take_branch = (unsigned_32(register[rs1]) < unsigned_32(register[rs2]))
+    elif func3 == 0x7: take_branch = (unsigned_32(register[rs1]) >= unsigned_32(register[rs2]))
+
+    if take_branch:
+        next_pc = (pc + imm) & 0xFFFFFFFF
+        if next_pc == pc:
+            halt = True
 
